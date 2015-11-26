ec61e991aed2b5e5e6b65ee26544317fa67a63b3
Luis Juan Sanchez <luisjuansp@gmail.com>
2015-11-25 22:01:40 -0600

    2,3,5,6

--- tarea7.pl
+++ tarea7.pl
@@ -1,43 +1,47 @@
-%Primero
+%PRIMERO
 
 divisible(X,Y) :- 0 is X mod Y, !.
 
-divisible(X,Y) :- X > Y+1, divisible(X, Y+1).
+divisible(X,Y) :- Y1 is Y+1, X > Y1, divisible(X, Y1).
 
 primo(2) :- true,!. 
 primo(X) :- X < 2,!,false.
-primo(X) :- not(divisible(X, 2)).
+primo(X) :- \+(divisible(X, 2)).
 
-%Segundo
+%SEGUNDO
 
-primos(2).
-primos(3).
-primos(P) :- 
-    integer(P), 
-    P > 3, 
-    P mod 2 =\= 0, 
-    \+ has_factor(P,3).  
+primos(X, Y, [X|L]) :- X =< Y, primo(X), !, X1 is X+1, primos(X1, Y, L).
+primos(X, Y, L) :- X =< Y, !, X1 is X+1, primos(X1, Y, L).
+primos(_, _, []).
 
-has_factor(N,L) :- N mod L =:= 0.
-has_factor(N,L) :- 
-    L * L < N, 
-    L2 is L + 2, 
-    has_factor(N,L2).
+%TERCERO
 
-prime_list(A,B,L) :- 
-    A1 is (A // 2) * 2 + 1, 
-    p_list(A1,B,L).
+comprime([X,Y|R], [X,Y|R1]) :- \+ X = Y, !, comprime([Y|R], [Y|R1]).
+comprime([X,_|R], [X|R1]) :- !, comprime(R, R1).
+comprime([X|R], [X|R1]) :- !, comprime(R, R1).
+comprime([], []).
 
+%CUARTO
 
-p_list(A,B,[]) :- A > B, !.
-p_list(A,B,[A|L]) :- 
-    primos(A), !, 
-    next(A,A1), 
-    p_list(A1,B,L).
+%QUINTO
 
-p_list(A,B,L) :- 
-    next(A,A1), 
-    p_list(A1,B,L).
+decodifica([[N,E]|R], L1) :- !, repite(E, N, L), decodifica(R, R1), append(L, R1, L1).
+decodifica([], []).
 
-next(2,3) :- !.
-next(A,A1) :- A1 is A + 2.
+repite(X, N, [X|R]) :- N > 0, !, N1 is N-1, repite(X, N1, R).
+repite(_, 0, []).
+
+%SEXTO
+
+elimren(R, [X| M], [X|M1]) :- R > 1, !, R1 is R-1, elimren(R1, M, M1).
+elimren(R, [_| M], M) :- R = 1, !.
+elimren(R, [X|M], [X|M]) :- R < 1, !.
+elimren(_, [], []).
+
+%SEPTIMO
+
+%OCTAVO
+
+%NOVENO
+
+%DECIMO
